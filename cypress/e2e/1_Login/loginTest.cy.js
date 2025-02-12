94b0073103fce7bd71c29ab4af5e5d65ae5c7338
ajinkyap738 <ajinkyap738@gmail.com>
2025-02-12 19:12:19 +0530

    Added the test fixture json file for data driver

--- cypress/e2e/1_Login/loginTest.cy.js
+++ cypress/e2e/1_Login/loginTest.cy.js
@@ -4,7 +4,7 @@ describe("Login Test suite",()=>{
 it("Login with valid credentials",()=>{
     cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
     cy.url().should('include',"demo")
-    cy.title().should('contains',"Orange")
+    cy.title().should('contgit initains',"Orange")
     const aj=new Login();
     aj.SetUsername("Admin")
     aj.SetPassword("admin123");
@@ -12,5 +12,16 @@ it("Login with valid credentials",()=>{
     aj.VerifyDashboard();
 
 })
-
+it.only("Login with valid credentials by using data file",()=>{
+    cy.visit("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    cy.url().should('include',"demo")
+    cy.title().should('contains',"Orange")
+    cy.fixture("testData.json").then(function(signInData){
+    const aj=new Login();
+    aj.SetUsername(signInData.username)
+    aj.SetPassword(signInData.password);
+    aj.ClickLogin();
+    aj.VerifyDashboard();
+    })
+})
 })
